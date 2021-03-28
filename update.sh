1fc4beef675f9e7312638a7de83a765d8a4118e7
Seth Troisi <sethtroisi@google.com>
2021-03-28 03:52:09 -0700

    Add note in update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,8 @@ cd _data
 # Arg 1 or default path
 PRIME_GAP_LIST_DIR="${1:-../../prime-gap-list}"
 
-ls -sh "$PRIME_GAP_LIST_DIR/allgaps.sql"
+echo -e "\nDon't forget to git pull in $PRIME_GAP_LIST_DIR\n"
+ls -1h "$PRIME_GAP_LIST_DIR/allgaps.sql"
 
 # Assumes prime-gap-list checked out at same height as primegap-list-project.github.io
 rm -f "$PRIME_GAP_LIST_DIR/gaps.db"
